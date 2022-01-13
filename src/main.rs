857906199b40b7b37ddf67f644a1c185a36da393
ujjwal-kr <epicujjwal@protonmail.com>
2022-01-13 19:40:26 +0530

    add add command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let mut program: Vec<&str> = vec![];
 
     program.push("push 1000");
+    program.push("push 500");
+    program.push("add");
     program.push("print");
 
     println!("{:?}", program);
@@ -9,19 +11,18 @@ fn main() {
 }
 
 fn read_statement(program: Vec<&str>) {
-    let mut stack: Vec<&str> = vec![];
+    let mut stack: Vec<String> = vec![];
 
     for statement in program {
         let cmd: Vec<&str> = statement.split(" ").collect();
 
         match cmd[0] {
             "print" => println!("{}", stack[stack.len() - 1]),
-            "push" => stack.push(cmd[1]),
+            "push" => stack.push(cmd[1].to_string()),
             "pop" => { stack.pop(); },
             "add" => {
-                let result = stack[stack.len()].parse::<i32>().unwrap() + stack[stack.len() - 1].parse::<i32>().unwrap();
-                let result_str = result.to_string();
-                stack.push(result_str);
+                let result = stack[stack.len() - 1].parse::<i32>().unwrap() + stack[stack.len() - 2].parse::<i32>().unwrap();
+                stack.push(result.to_string());
             },
             _ => println!("Program End")
         }
