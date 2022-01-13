0e9760e593d03668d97dfab3589129749a53a5f1
ujjwal-kr <epicujjwal@protonmail.com>
2022-01-13 18:28:55 +0530

    add some commands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 fn main() {
-    let mut stack: Vec<&str> = vec![];
-    
-    stack.push("push 10");
-    stack.push("push 12");
+    let mut program: Vec<&str> = vec![];
 
-    println!("{:?}", stack);
-    read_statement(stack);
+    program.push("push 1000");
+    program.push("print");
+
+    println!("{:?}", program);
+    read_statement(program);
 }
 
 fn read_statement(program: Vec<&str>) {
-    for command in program {
-        let cmd = command.split(" ");
-        println!("{:?}", cmd);
+    let mut stack: Vec<&str> = vec![];
+
+    for statement in program {
+        let cmd: Vec<&str> = statement.split(" ").collect();
+
+        match cmd[0] {
+            "print" => println!("{}", stack[stack.len() - 1]),
+            "push" => stack.push(cmd[1]),
+            "pop" => { stack.pop(); },
+            "add" => {
+                let result = stack[stack.len()].parse::<i32>().unwrap() + stack[stack.len() - 1].parse::<i32>().unwrap();
+                let result_str = result.to_string();
+                stack.push(result_str);
+            },
+            _ => println!("Program End")
+        }
     }
 }
+
