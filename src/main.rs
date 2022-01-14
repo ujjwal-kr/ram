988b16fed5f0cabf6aa5dd6d6c056f3fbcf73b0d
ujjwal-kr <epicujjwal@protonmail.com>
2022-01-14 06:41:04 +0530

    add more formulas

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::io;
+use std::f64;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> 
@@ -19,17 +20,17 @@ fn main() -> std::io::Result<()>
     Ok(())
 }
 
-fn run_statement(program: &Vec<&str>) 
-{
-    let mut stack: Vec<i32> = vec![];
+fn run_statement(program: &Vec<&str>) {
+    let mut stack: Vec<f64> = vec![];
 
     for statement in program {
         let cmd: Vec<&str> = statement.split(" ").collect();
 
         match cmd[0] {
             "print" => println!("{}", stack[stack.len() - 1]),
-            "push" => stack.push(cmd[1].parse::<i32>().unwrap()),
+            "ram" => stack.push(cmd[1].parse::<f64>().unwrap()),
             "pop" => { stack.pop(); },
+            "popall"  => { stack = vec![] }
             "add" => {
                 let result = stack[stack.len() - 1] + stack[stack.len() - 2];
                 stack.push(result);
@@ -39,21 +40,41 @@ fn run_statement(program: &Vec<&str>)
                 stack.push(result);
             },
 
-            "cmp" => {
-                if stack[stack.len() -2] == stack[stack.len() - 1]
-                {
-                    stack.push(0);
-                } else if stack[stack.len() -2] < stack[stack.len() - 1]
-                    {
-                        stack.push(-1)
-                    }
-                  else if stack[stack.len() -2] > stack[stack.len() - 1]
-                    {
-                        stack.push(1)                        
-                    }  
+            "mul" => {
+                let result = stack[stack.len() -1] * stack[stack.len() -2];
+                stack.push(result);
+            }
+            
+            "div" => {
+                let result = stack[stack.len() - 2] / stack[stack.len() -1];
+                stack.push(result)
             },
+
+            "sqr" => {
+                let result = stack[stack.len() -1] * stack[stack.len() - 1];
+                stack.push(result);
+            }
+
+            "sqrt" => {
+                let result = stack[stack.len() - 1].sqrt();
+                stack.push(result);
+            }
+
+            "round" => {
+                let result = stack[stack.len() - 1].round();
+                stack.push(result);
+            },
+            
+            "avg" => {
+                let mut total: f64 = 0.0;
+                let mut i: f64 = 0.0;
+                for num in &stack {
+                    i = i + 1.0;
+                    total = total + num
+                }
+                stack.push(total / i)
+            }
             _ => { println!("Cant recongize command '{}'", cmd[0]); break }
         }
     }
 }
-
