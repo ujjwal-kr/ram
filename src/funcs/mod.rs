3aa40faaeecfa2ce0d4ad27ede97ac2a9f4d04c5
Ujjwal Kumar <katanadeveloper01@gmail.com>
2022-02-06 17:38:11 +0000

    separate basic ops

--- src/funcs/mod.rs
+++ src/funcs/mod.rs
@@ -1,3 +1,4 @@
 pub mod errors;
 pub mod print;
 pub mod stack;
+pub mod operations;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io;
 use std::io::prelude::*;
 
 mod funcs;
-use funcs::{errors, print, stack};
+use funcs::{errors, print, stack, operations};
 
 #[derive(Clone)]
 pub struct Vars {
@@ -83,16 +83,16 @@ fn run_statement(
             "stdin" => stack::stdin(&mut local_vars, cmd, block_number, line),
             "pop" => stack::pop(&mut stack, block_number, line),
             "popall" => stack = vec![],
-            "add" => add(&mut stack, cmd, &mut local_vars, block_number, line),
-            "sub" => sub(&mut stack, block_number, line),
-            "mul" => mul(&mut stack, cmd, &mut local_vars, block_number, line),
-            "div" => div(&mut stack, block_number, line),
-            "sqr" => sqr(&mut stack, cmd, &mut local_vars, block_number, line),
-            "sqrt" => sqrt(&mut stack, cmd, &mut local_vars, block_number, line),
-            "round" => round(&mut stack, cmd, &mut local_vars, block_number, line),
-            "avg" => avg(&mut stack, block_number, line),
+            "add" => operations::add(&mut stack, cmd, &mut local_vars, block_number, line),
+            "sub" => operations::sub(&mut stack, block_number, line),
+            "mul" => operations::mul(&mut stack, cmd, &mut local_vars, block_number, line),
+            "div" => operations::div(&mut stack, block_number, line),
+            "sqr" => operations::sqr(&mut stack, cmd, &mut local_vars, block_number, line),
+            "sqrt" => operations::sqrt(&mut stack, cmd, &mut local_vars, block_number, line),
+            "round" => operations::round(&mut stack, cmd, &mut local_vars, block_number, line),
+            "avg" => operations::avg(&mut stack, block_number, line),
             "rand" => stack::random(&mut stack, statement, block_number, line),
-            "cmp" => cmp(&mut stack, block_number, line),
+            "cmp" => operations::cmp(&mut stack, block_number, line),
 
             "je" => {
                 if cmd.len() != 2 {
@@ -203,126 +203,4 @@ fn run_statement(
         }
     }
     Ok(())
-}
-
-fn add(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
-    if cmd.len() > 1 {
-        stack.push(vars.lx + vars.rv);
-    } else {
-        if stack.len() < 2 {
-            errors::stack_len_error(b, l);
-        } else {
-            let result = stack[stack.len() - 1] + stack[stack.len() - 2];
-            stack.push(result);
-        }
-    }
-}
-
-fn sub(stack: &mut Vec<f64>, b: usize, l: u32) {
-    if stack.len() < 2 {
-        errors::stack_len_error(b, l);
-    }
-    let result = stack[stack.len() - 2] - stack[stack.len() - 1];
-    stack.push(result);
-}
-
-fn mul(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
-    if cmd.len() > 1 {
-        stack.push(vars.lx * vars.rv);
-    } else {
-        if stack.len() < 2 {
-            errors::stack_len_error(b, l);
-        }
-        let result = stack[stack.len() - 1] * stack[stack.len() - 2];
-        stack.push(result);
-    }
-}
-
-fn div(stack: &mut Vec<f64>, b: usize, l: u32) {
-    if stack.len() < 2 {
-        errors::stack_len_error(b, l);
-    }
-    let result = stack[stack.len() - 2] / stack[stack.len() - 1];
-    stack.push(result)
-}
-
-fn sqr(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
-    if cmd[1] == "lx" || cmd[1] == "rv" {
-        if cmd[1] == "lx" {
-            vars.lx = vars.lx * vars.lx;
-        }
-        if cmd[1] == "rv" {
-            vars.rv = vars.rv * vars.rv;
-        }
-    } else {
-        if stack.len() < 1 {
-            errors::stack_len_error(b, l);
-        }
-        let result = stack[stack.len() - 1] * stack[stack.len() - 1];
-        stack.push(result);
-    }
-}
-
-fn sqrt(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
-    if cmd[1] == "lx" || cmd[1] == "rv" {
-        if cmd[1] == "lx" {
-            vars.lx = vars.lx.sqrt();
-        }
-        if cmd[1] == "rv" {
-            vars.rv = vars.rv.sqrt();
-        }
-    } else {
-        let result = stack[stack.len() - 1].sqrt();
-        stack.push(result);
-    }
-    if stack.len() < 1 {
-        errors::stack_len_error(b, l);
-    }
-    let result = stack[stack.len() - 1].sqrt();
-    stack.push(result);
-}
-
-fn round(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
-    if cmd.len() > 1 {
-        if cmd[1] == "lx" || cmd[1] == "rv" {
-            if cmd[1] == "lx" {
-                vars.lx = vars.lx.round();
-            }
-            if cmd[1] == "rv" {
-                vars.rv = vars.rv.round();
-            }
-        }
-    } else {
-        if stack.len() < 1 {
-            errors::stack_len_error(b, l);
-        }
-        let result = stack[stack.len() - 1].round();
-        stack.push(result);
-    }
-}
-
-fn avg(stack: &mut Vec<f64>, b: usize, l: u32) {
-    if stack.len() < 1 {
-        errors::stack_len_error(b, l);
-    }
-    let mut total: f64 = 0.0;
-    let mut i: f64 = 0.0;
-    for num in stack.clone() {
-        i = i + 1.0;
-        total = total + num
-    }
-    stack.push(total / i)
-}
-
-fn cmp(stack: &mut Vec<f64>, b: usize, l: u32) {
-    if stack.len() < 2 {
-        errors::stack_len_error(b, l);
-    }
-    if stack[stack.len() - 1] < stack[stack.len() - 2] {
-        stack.push(1.0)
-    } else if stack[stack.len() - 1] > stack[stack.len() - 2] {
-        stack.push(-1.0)
-    } else {
-        stack.push(0.0)
-    }
 }
