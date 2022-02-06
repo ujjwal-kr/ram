652907baa144d3f20d10446b7115c52ec9374088
Ujjwal Kumar <katanadeveloper01@gmail.com>
2022-02-06 03:11:28 +0000

    separate executions in functions and errors in another file

---
+++ src/errors.rs
@@ -0,0 +1,53 @@
+// Errors
+
+pub fn parse_float(arg: &str, block: usize, line: u32) -> f64 {
+    let num: f64;
+    num = arg.parse().expect(
+        format!(
+            "Input a number at block:{}:line{}",
+            block.to_string(),
+            line.to_string()
+        )
+        .trim(),
+    );
+    num
+}
+
+pub fn parse_usize(arg: &str, block: usize, line: u32) -> usize {
+    let num: usize;
+    num = arg.parse().expect(
+        format!(
+            "Invalid jmp statement, block {} is invalid block:{}::line:{}",
+            arg,
+            block.to_string(),
+            line.to_string()
+        )
+        .trim(),
+    );
+    num
+}
+
+pub fn stack_len_error(block: usize, line: u32) {
+    println!(
+        "Not enough items in the stack, block:{}::line:{}",
+        block.to_string(),
+        line.to_string()
+    );
+}
+
+pub fn args_error(block: usize, line: u32) {
+    println!(
+        "Invalid arguments at block:{}::line:{}",
+        block.to_string(),
+        line.to_string()
+    );
+}
+
+pub fn invalid_jmp(block: usize, line: u32, inv_block: usize) {
+    println!(
+        "Invalid jmp arguments at block:{}::line:{}, block {} does not exist",
+        block.to_string(),
+        line.to_string(),
+        inv_block.to_string()
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::fs;
 use std::io;
 use std::io::prelude::*;
 
+mod errors;
+use errors as errs;
+
 #[derive(Copy, Clone)]
 struct Vars {
     lx: f64,
@@ -56,208 +59,32 @@ fn run_statement(
         }
         match cmd[0] {
             "//" => (),
-            "print" => {
-                print(&mut stack, cmd, &mut local_vars, block_number, line);
-            }
+            "print" => print(&mut stack, cmd, &mut local_vars, block_number, line),
             "printc" => printc(cmd, statement, block_number, line),
-            "ram" => {
-                if cmd[1] == "lx" || cmd[1] == "rv" {
-                    if cmd.len() == 2 {
-                        if cmd[1] == "lx" {
-                            stack.push(local_vars.lx)
-                        }
-                        if cmd[1] == "rv" {
-                            stack.push(local_vars.rv)
-                        }
-                    } else {
-                        if cmd[1] == "lx" {
-                            if cmd[2] == "prev" {
-                                local_vars.lx = stack[stack.len() - 1];
-                            } else {
-                                local_vars.lx = parse_float(cmd[2], block_number, line)
-                            }
-                        }
-                        if cmd[1] == "rv" {
-                            if cmd[2] == "prev" {
-                                local_vars.rv = stack[stack.len() - 1];
-                            } else {
-                                local_vars.rv = parse_float(cmd[2], block_number, line)
-                            }
-                        }
-                    }
-                } else {
-                    stack.push(parse_float(cmd[1], block_number, line))
-                }
-            }
-            "stdin" => {
-                if cmd.len() != 2 {
-                    args_error(block_number, line);
-                    break;
-                }
-                let mut input = String::new();
-                io::stdin().read_line(&mut input)?;
-                let number: f64 = parse_float(input.trim(), block_number, line);
-                if cmd[1] == "lx" {
-                    local_vars.lx = number
-                }
-                if cmd[1] == "rv" {
-                    local_vars.rv = number
-                }
-            }
-            "pop" => {
-                pop(&mut stack, block_number, line);
-            }
+            "ram" => ram(&mut stack, cmd, &mut local_vars, block_number, line),
+            "stdin" => stdin(&mut local_vars, cmd, block_number, line),
+            "pop" => pop(&mut stack, block_number, line),
             "popall" => stack = vec![],
-            "add" => {
-                if cmd.len() > 1 {
-                    stack.push(local_vars.lx + local_vars.rv);
-                } else {
-                    if stack.len() < 2 {
-                        stack_len_error(block_number, line);
-                        break;
-                    }
-                    let result = stack[stack.len() - 1] + stack[stack.len() - 2];
-                    stack.push(result);
-                }
-            }
-            "sub" => {
-                if stack.len() < 2 {
-                    stack_len_error(block_number, line);
-                    break;
-                }
-                let result = stack[stack.len() - 2] - stack[stack.len() - 1];
-                stack.push(result);
-            }
-
-            "mul" => {
-                if cmd.len() > 1 {
-                    stack.push(local_vars.lx * local_vars.rv);
-                } else {
-                    if stack.len() < 2 {
-                        stack_len_error(block_number, line);
-                        break;
-                    }
-                    let result = stack[stack.len() - 1] * stack[stack.len() - 2];
-                    stack.push(result);
-                }
-            }
-
-            "div" => {
-                if stack.len() < 2 {
-                    stack_len_error(block_number, line);
-                    break;
-                }
-                let result = stack[stack.len() - 2] / stack[stack.len() - 1];
-                stack.push(result)
-            }
-
-            "sqr" => {
-                if cmd[1] == "lx" || cmd[1] == "rv" {
-                    if cmd[1] == "lx" {
-                        local_vars.lx = local_vars.lx * local_vars.lx;
-                    }
-                    if cmd[1] == "rv" {
-                        local_vars.rv = local_vars.rv * local_vars.rv;
-                    }
-                } else {
-                    if stack.len() < 1 {
-                        stack_len_error(block_number, line);
-                        break;
-                    }
-                    let result = stack[stack.len() - 1] * stack[stack.len() - 1];
-                    stack.push(result);
-                }
-            }
-
-            "sqrt" => {
-                if cmd[1] == "lx" || cmd[1] == "rv" {
-                    if cmd[1] == "lx" {
-                        local_vars.lx = local_vars.lx.sqrt();
-                    }
-                    if cmd[1] == "rv" {
-                        local_vars.rv = local_vars.rv.sqrt();
-                    }
-                } else {
-                    let result = stack[stack.len() - 1].sqrt();
-                    stack.push(result);
-                }
-                if stack.len() < 1 {
-                    stack_len_error(block_number, line);
-                    break;
-                }
-                let result = stack[stack.len() - 1].sqrt();
-                stack.push(result);
-            }
-
-            "round" => {
-                if cmd.len() > 1 {
-                    if cmd[1] == "lx" || cmd[1] == "rv" {
-                        if cmd[1] == "lx" {
-                            local_vars.lx = local_vars.lx.round();
-                        }
-                        if cmd[1] == "rv" {
-                            local_vars.rv = local_vars.rv.round();
-                        }
-                    }
-                } else {
-                    if stack.len() < 1 {
-                        stack_len_error(block_number, line);
-                        break;
-                    }
-                    let result = stack[stack.len() - 1].round();
-                    stack.push(result);
-                }
-            }
-
-            "avg" => {
-                if stack.len() < 1 {
-                    stack_len_error(block_number, line);
-                    break;
-                }
-                let mut total: f64 = 0.0;
-                let mut i: f64 = 0.0;
-                for num in &stack {
-                    i = i + 1.0;
-                    total = total + num
-                }
-                stack.push(total / i)
-            }
-
-            "rand" => {
-                let rand_cmd: Vec<&str> = statement.split(">>").collect();
-                let numbers: Vec<&str> = rand_cmd[1].split(",").collect();
-
-                let mut rng = rand::thread_rng();
-                let random = rng.gen_range(
-                    parse_float(numbers[0].trim(), block_number, line)
-                        ..parse_float(numbers[1].trim(), block_number, line),
-                );
-                stack.push(random);
-            }
-
-            "cmp" => {
-                if stack.len() < 2 {
-                    stack_len_error(block_number, line);
-                    break;
-                }
-                if stack[stack.len() - 1] < stack[stack.len() - 2] {
-                    stack.push(1.0)
-                } else if stack[stack.len() - 1] > stack[stack.len() - 2] {
-                    stack.push(-1.0)
-                } else {
-                    stack.push(0.0)
-                }
-            }
+            "add" => add(&mut stack, cmd, &mut local_vars, block_number, line),
+            "sub" => sub(&mut stack, block_number, line),
+            "mul" => mul(&mut stack, cmd, &mut local_vars, block_number, line),
+            "div" => div(&mut stack, block_number, line),
+            "sqr" => sqr(&mut stack, cmd, &mut local_vars, block_number, line),
+            "sqrt" => sqrt(&mut stack, cmd, &mut local_vars, block_number, line),
+            "round" => round(&mut stack, cmd, &mut local_vars, block_number, line),
+            "avg" => avg(&mut stack, block_number, line),
+            "rand" => random(&mut stack, statement, block_number, line),
+            "cmp" => cmp(&mut stack, block_number, line),
 
             "je" => {
                 if cmd.len() != 2 {
-                    args_error(block_number, line);
+                    errs::args_error(block_number, line);
                     break;
                 }
                 if stack[stack.len() - 1] == 0.0 {
-                    let index: usize = parse_usize(cmd[1], block_number, line);
+                    let index: usize = errs::parse_usize(cmd[1], block_number, line);
                     if blocks.len() <= index {
-                        invalid_jmp(block_number, line, index);
+                        errs::invalid_jmp(block_number, line, index);
                         break;
                     }
                     match run_statement(blocks, &blocks[index], index, local_vars) {
@@ -271,13 +98,13 @@ fn run_statement(
 
             "jne" => {
                 if cmd.len() != 2 {
-                    args_error(block_number, line);
+                    errs::args_error(block_number, line);
                     break;
                 }
                 if stack[stack.len() - 1] != 0.0 {
-                    let index: usize = parse_usize(cmd[1], block_number, line);
+                    let index: usize = errs::parse_usize(cmd[1], block_number, line);
                     if blocks.len() <= index {
-                        invalid_jmp(block_number, line, index);
+                        errs::invalid_jmp(block_number, line, index);
                         break;
                     }
                     match run_statement(blocks, &blocks[index], index, local_vars) {
@@ -291,13 +118,13 @@ fn run_statement(
 
             "jgr" => {
                 if cmd.len() != 2 {
-                    args_error(block_number, line);
+                    errs::args_error(block_number, line);
                     break;
                 }
                 if stack[stack.len() - 1] == 1.0 {
-                    let index: usize = parse_usize(cmd[1], block_number, line);
+                    let index: usize = errs::parse_usize(cmd[1], block_number, line);
                     if blocks.len() <= index {
-                        invalid_jmp(block_number, line, index);
+                        errs::invalid_jmp(block_number, line, index);
                         break;
                     }
                     match run_statement(blocks, &blocks[index], index, local_vars) {
@@ -311,13 +138,13 @@ fn run_statement(
 
             "jsm" => {
                 if cmd.len() != 2 {
-                    args_error(block_number, line);
+                    errs::args_error(block_number, line);
                     break;
                 }
                 if stack[stack.len() - 1] == -1.0 {
-                    let index: usize = parse_usize(cmd[1], block_number, line);
+                    let index: usize = errs::parse_usize(cmd[1], block_number, line);
                     if blocks.len() <= index {
-                        invalid_jmp(block_number, line, index)
+                        errs::invalid_jmp(block_number, line, index)
                     }
                     match run_statement(blocks, &blocks[index], index, local_vars) {
                         Ok(()) => (),
@@ -330,12 +157,12 @@ fn run_statement(
 
             "jmp" => {
                 if cmd.len() != 2 {
-                    args_error(block_number, line);
+                    errs::args_error(block_number, line);
                     break;
                 }
-                let index: usize = parse_usize(cmd[1], block_number, line);
+                let index: usize = errs::parse_usize(cmd[1], block_number, line);
                 if blocks.len() <= index {
-                    invalid_jmp(block_number, line, index);
+                    errs::invalid_jmp(block_number, line, index);
                     break;
                 }
                 match run_statement(blocks, &blocks[index], index, local_vars) {
@@ -360,12 +187,10 @@ fn run_statement(
     Ok(())
 }
 
-// Functions
-
 fn print(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
     if cmd.len() == 1 {
         if stack.len() < 1 {
-            stack_len_error(b, l);
+            errs::stack_len_error(b, l);
         } else {
             println!("{}", stack[stack.len() - 1]);
         }
@@ -381,7 +206,7 @@ fn print(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32
 
 fn printc(cmd: Vec<&str>, statement: &str, b: usize, l: u32) {
     if cmd.len() < 3 {
-        args_error(b, l);
+        errs::args_error(b, l);
     } else {
         let prntc_cmd: Vec<&str> = statement.split(">>").collect();
         println!("{}", prntc_cmd[1].trim());
@@ -390,61 +215,188 @@ fn printc(cmd: Vec<&str>, statement: &str, b: usize, l: u32) {
 
 fn pop(stack: &mut Vec<f64>, b: usize, l: u32) {
     if stack.len() < 1 {
-        stack_len_error(b, l);
+        errs::stack_len_error(b, l);
     }
     stack.pop();
 }
 
-// Errors
-
-fn parse_float(arg: &str, block: usize, line: u32) -> f64 {
-    let num: f64;
-    num = arg.parse().expect(
-        format!(
-            "Input a number at block:{}:line{}",
-            block.to_string(),
-            line.to_string()
-        )
-        .trim(),
-    );
-    num
+fn ram(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
+    if cmd[1] == "lx" || cmd[1] == "rv" {
+        if cmd.len() == 2 {
+            if cmd[1] == "lx" {
+                stack.push(vars.lx)
+            }
+            if cmd[1] == "rv" {
+                stack.push(vars.rv)
+            }
+        } else {
+            if cmd[1] == "lx" {
+                if cmd[2] == "prev" {
+                    vars.lx = stack[stack.len() - 1];
+                } else {
+                    vars.lx = errs::parse_float(cmd[2], b, l)
+                }
+            }
+            if cmd[1] == "rv" {
+                if cmd[2] == "prev" {
+                    vars.rv = stack[stack.len() - 1];
+                } else {
+                    vars.rv = errs::parse_float(cmd[2], b, l)
+                }
+            }
+        }
+    } else {
+        stack.push(errs::parse_float(cmd[1], b, l))
+    }
 }
 
-fn parse_usize(arg: &str, block: usize, line: u32) -> usize {
-    let num: usize;
-    num = arg.parse().expect(
-        format!(
-            "Invalid jmp statement, block {} is invalid block:{}::line:{}",
-            arg,
-            block.to_string(),
-            line.to_string()
-        )
-        .trim(),
-    );
-    num
+fn stdin(vars: &mut Vars, cmd: Vec<&str>, b: usize, l: u32) {
+    if cmd.len() != 2 {
+        errs::args_error(b, l);
+    } else {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("something went wrong");
+        let number: f64 = errs::parse_float(input.trim(), b, l);
+        if cmd[1] == "lx" {
+            vars.lx = number
+        }
+        if cmd[1] == "rv" {
+            vars.rv = number
+        }
+    }
 }
 
-fn stack_len_error(block: usize, line: u32) {
-    println!(
-        "Not enough items in the stack, block:{}::line:{}",
-        block.to_string(),
-        line.to_string()
-    );
+fn add(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
+    if cmd.len() > 1 {
+        stack.push(vars.lx + vars.rv);
+    } else {
+        if stack.len() < 2 {
+            errs::stack_len_error(b, l);
+        } else {
+            let result = stack[stack.len() - 1] + stack[stack.len() - 2];
+            stack.push(result);
+        }
+    }
 }
 
-fn args_error(block: usize, line: u32) {
-    println!(
-        "Invalid arguments at block:{}::line:{}",
-        block.to_string(),
-        line.to_string()
-    );
+fn sub(stack: &mut Vec<f64>, b: usize, l: u32) {
+    if stack.len() < 2 {
+        errs::stack_len_error(b, l);
+    }
+    let result = stack[stack.len() - 2] - stack[stack.len() - 1];
+    stack.push(result);
 }
 
-fn invalid_jmp(block: usize, line: u32, inv_block: usize) {
-    println!(
-        "Invalid jmp arguments at block:{}::line:{}, block {} does not exist",
-        block.to_string(),
-        line.to_string(),
-        inv_block.to_string()
+fn mul(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
+    if cmd.len() > 1 {
+        stack.push(vars.lx * vars.rv);
+    } else {
+        if stack.len() < 2 {
+            errs::stack_len_error(b, l);
+        }
+        let result = stack[stack.len() - 1] * stack[stack.len() - 2];
+        stack.push(result);
+    }
+}
+
+fn div(stack: &mut Vec<f64>, b: usize, l: u32) {
+    if stack.len() < 2 {
+        errs::stack_len_error(b, l);
+    }
+    let result = stack[stack.len() - 2] / stack[stack.len() - 1];
+    stack.push(result)
+}
+
+fn sqr(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
+    if cmd[1] == "lx" || cmd[1] == "rv" {
+        if cmd[1] == "lx" {
+            vars.lx = vars.lx * vars.lx;
+        }
+        if cmd[1] == "rv" {
+            vars.rv = vars.rv * vars.rv;
+        }
+    } else {
+        if stack.len() < 1 {
+            errs::stack_len_error(b, l);
+        }
+        let result = stack[stack.len() - 1] * stack[stack.len() - 1];
+        stack.push(result);
+    }
+}
+
+fn sqrt(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
+    if cmd[1] == "lx" || cmd[1] == "rv" {
+        if cmd[1] == "lx" {
+            vars.lx = vars.lx.sqrt();
+        }
+        if cmd[1] == "rv" {
+            vars.rv = vars.rv.sqrt();
+        }
+    } else {
+        let result = stack[stack.len() - 1].sqrt();
+        stack.push(result);
+    }
+    if stack.len() < 1 {
+        errs::stack_len_error(b, l);
+    }
+    let result = stack[stack.len() - 1].sqrt();
+    stack.push(result);
+}
+
+fn round(stack: &mut Vec<f64>, cmd: Vec<&str>, vars: &mut Vars, b: usize, l: u32) {
+    if cmd.len() > 1 {
+        if cmd[1] == "lx" || cmd[1] == "rv" {
+            if cmd[1] == "lx" {
+                vars.lx = vars.lx.round();
+            }
+            if cmd[1] == "rv" {
+                vars.rv = vars.rv.round();
+            }
+        }
+    } else {
+        if stack.len() < 1 {
+            errs::stack_len_error(b, l);
+        }
+        let result = stack[stack.len() - 1].round();
+        stack.push(result);
+    }
+}
+
+fn avg(stack: &mut Vec<f64>, b: usize, l: u32) {
+    if stack.len() < 1 {
+        errs::stack_len_error(b, l);
+    }
+    let mut total: f64 = 0.0;
+    let mut i: f64 = 0.0;
+    for num in stack.clone() {
+        i = i + 1.0;
+        total = total + num
+    }
+    stack.push(total / i)
+}
+
+fn random(stack: &mut Vec<f64>, statement: &str, b: usize, l: u32) {
+    let rand_cmd: Vec<&str> = statement.split(">>").collect();
+    let numbers: Vec<&str> = rand_cmd[1].split(",").collect();
+
+    let mut rng = rand::thread_rng();
+    let random = rng.gen_range(
+        errs::parse_float(numbers[0].trim(), b, l)..errs::parse_float(numbers[1].trim(), b, l),
     );
+    stack.push(random);
+}
+
+fn cmp(stack: &mut Vec<f64>, b: usize, l: u32) {
+    if stack.len() < 2 {
+        errs::stack_len_error(b, l);
+    }
+    if stack[stack.len() - 1] < stack[stack.len() - 2] {
+        stack.push(1.0)
+    } else if stack[stack.len() - 1] > stack[stack.len() - 2] {
+        stack.push(-1.0)
+    } else {
+        stack.push(0.0)
+    }
 }
