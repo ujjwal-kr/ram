789569ea73abd6fa734d864695e80209b3a1ab68
Ujjwal <epicujjwal@protonmail.com>
2022-08-10 14:49:13 +0530

    includes: remove redundant types

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn main() -> std::io::Result<()> {
     file.read_to_string(&mut contents)?;
 
     let p_lines: Vec<&str> = contents.split("\n").collect();
-    let import_mappings: parser::MappedProgram = parser::traverse_includes(filename);
+    let import_mappings: HashMap<String, Vec<&str>> = parser::traverse_includes(filename);
     let program: HashMap<String, Vec<String>> = parser::populate_labels(p_lines);
 
     let mut stack: Vec<f64> = vec![];
--- src/parser.rs
+++ src/parser.rs
@@ -3,20 +3,8 @@ use std::collections::HashMap;
 use std::io::prelude::*;
 use std::{fs, io, path::Path, process};
 
-#[derive(Debug)]
-pub struct IncludeMapping {
-    pub filename: String,
-    pub has_included: Vec<String>,
-}
-
-#[derive(Debug)]
-pub struct MappedProgram<'a> {
-    pub p_lines: Vec<&'a str>,
-    pub map: HashMap<String, Vec<String>>,
-}
-
 pub fn populate_labels(p_lines: Vec<&str>) -> HashMap<String, Vec<String>> {
-    file_map_includes(p_lines.clone(), "main.rsm".to_string());
+    file_map_includes(p_lines.clone());
     let mut program: HashMap<String, Vec<String>> = HashMap::new();
     let mut current_key: String = String::new();
     let exp = Regex::new(r"^[a-zA-Z0-9_]+:$").unwrap();
@@ -56,9 +44,9 @@ pub fn populate_labels(p_lines: Vec<&str>) -> HashMap<String, Vec<String>> {
 // IMPORTS
 
 // opens up files by discovering them through file_map_includes and traversing through them
-pub fn traverse_includes(filename: String) -> MappedProgram<'static> {
+pub fn traverse_includes(filename: String) ->  HashMap<String, Vec<&'static str>> {
     let mut final_lines: Vec<String> = vec![];
-    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    let mut map: HashMap<String, Vec<&str>> = HashMap::new();
 
     // open the file
     let mut file = fs::File::open(filename.trim())
@@ -69,32 +57,26 @@ pub fn traverse_includes(filename: String) -> MappedProgram<'static> {
         .expect(format!("Error reading {}", filename).trim());
 
     let p_lines: Vec<&str> = contents.split("\n").collect();
-    let file_map: IncludeMapping = file_map_includes(p_lines, filename);
+    let file_map: Vec<&str> = file_map_includes(p_lines);
     println!("{:?}", file_map);
     
 
-    MappedProgram {
-        p_lines: vec![],
-        map: map,
-    }
+    map
 }
 
 // takes in all the lines and send IncludeMapping of a file
-pub fn file_map_includes(p_lines: Vec<&str>, filename: String) -> IncludeMapping {
-    let mut has_included: Vec<String> = vec![];
+pub fn file_map_includes(p_lines: Vec<&str>) -> Vec<&str> {
+    let mut has_included: Vec<&str> = vec![];
     for mut line in p_lines {
         line = line.trim();
         if line != "" {
             let cmd = line.split_whitespace().collect::<Vec<&str>>();
             if cmd[0] == "include" {
                 let includes = &cmd[1].split("\"").collect::<Vec<&str>>()[1];
-                has_included.push(includes.to_string());
+                has_included.push(includes);
             }
         }
     }
 
-    IncludeMapping {
-        filename: filename,
-        has_included: has_included,
-    }
+    has_included
 }
