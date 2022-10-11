b03be8243cf11dad1e1040ca4f102380e7dc32ca
Ujjwal <epicujjwal@protonmail.com>
2022-10-11 18:46:36 +0530

    print: add int and string yeild print using get_type

--- src/funcs/print.rs
+++ src/funcs/print.rs
@@ -1,5 +1,6 @@
 use crate::memory::Memory;
-use crate::{Registers, Vars};
+use crate::types::{Type, TypeName, Vars};
+use crate::Registers;
 
 pub fn print(
     memory: &mut Memory,
@@ -21,6 +22,13 @@ pub fn print(
             "lxstring" => println!("{}", registers.lxstring),
             _ => {
                 if cmd.len() == 3 {
+                    let _type: Type = vars.get_type(cmd[2].to_string(), b, l);
+                    match _type.name {
+                        TypeName::I32 => println!("{}", memory.yeild_i32(_type.location)),
+                        TypeName::String => println!("{}", memory.yeild_string(_type.location)),
+                        TypeName::Vector => todo!("Need to identify vector types"),
+                        _ => todo!("map for other types"),
+                    }
                 } else {
                     let print_st = &cmd[1..cmd.len()].to_vec().join(" ").to_string();
                     println!("{print_st}");
