104d23208b25c88ac4d646cc79ff3b201bc18535
Ujjwal <epicujjwal@protonmail.com>
2022-10-19 17:48:28 +0530

    main: move exec to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,165 @@
+pub mod funcs;
 pub mod memory;
 pub mod parser;
+pub mod tests;
 pub mod types;
+
+use funcs::*;
+use memory::Memory;
+use types::Vars;
+
+use std::collections::HashMap;
+use std::process;
+
+#[derive(Debug, Clone)]
+pub struct Registers {
+    pub lx: i32,
+    pub rv: i32,
+    pub string: String,
+    pub lxstring: String,
+}
+
+impl Registers {
+    pub fn new() -> Self {
+        Self {
+            lx: 0,
+            rv: 0,
+            string: String::new(),
+            lxstring: String::new(),
+        }
+    }
+}
+
+pub fn execute_block(
+    instructions: HashMap<String, Vec<String>>,
+    run_label: &str,
+    registers: Registers,
+    memory: &mut Memory,
+    vars: &mut Vars,
+) {
+    let mut line = 0i32;
+
+    let run_block: Vec<String>;
+    match instructions.get(run_label) {
+        Some(value) => run_block = value.to_vec(),
+        _ => {
+            println!("label '{}' not found", run_label);
+            process::exit(1);
+        }
+    }
+
+    let mut local_registers = Registers {
+        lx: registers.lx,
+        rv: registers.rv,
+        string: registers.string,
+        lxstring: registers.lxstring,
+    };
+
+    let block_ref = run_block.clone();
+
+    for statement in run_block {
+        let statement = statement.trim();
+        line += 1;
+        let cmd: Vec<&str> = statement.split_whitespace().collect();
+        match cmd[0] {
+            "dbg" => {
+                println!("Stack: ");
+                for i in memory.stack.chunks(8) {
+                    println!("{:?}", i)
+                }
+                println!(
+                    "{:#?} \n {:#?} \n HEAP: {:#?}",
+                    vars, local_registers, memory.heap
+                );
+
+                println!("{:?}", block_ref);
+            }
+            "print" => print::print(memory, vars, &mut local_registers, cmd, run_label, line),
+            "ram" => stack::ram(
+                memory,
+                vars,
+                &mut local_registers,
+                cmd,
+                statement,
+                run_label,
+                line,
+            ),
+            // "reset" => memory.reset_stack(),
+            // "move" => var::movefn(cmd, &mut local_vars, hash_vars, run_label, line),
+            // "str" => stack::strfn(&mut stack, &mut local_vars, cmd, run_label, line),
+            // "stdin" => stdfn::stdin(&mut local_vars, cmd, run_label, line),
+            // "stdfs" => stdfn::stdfs(&mut local_vars, cmd, statement, run_label, line),
+            // "pop" => stack::pop(&mut stack, cmd, run_label, line),
+            // // "popall" => stack = vec![],
+            // "add" => operations::add(&mut stack, cmd, &mut local_vars, run_label, line),
+            // "sub" => operations::sub(&mut stack, run_label, line),
+            // "mul" => operations::mul(&mut stack, cmd, &mut local_vars, run_label, line),
+            // "div" => operations::div(&mut stack, run_label, line),
+            // "sqr" => operations::sqr(&mut stack, cmd, &mut local_vars, run_label, line),
+            // "sqrt" => operations::sqrt(&mut stack, cmd, &mut local_vars, run_label, line),
+            // "round" => operations::round(&mut stack, cmd, &mut local_vars, run_label, line),
+            // "avg" => operations::avg(&mut stack, run_label, line),
+            // "rand" => stdfn::random(&mut local_vars, cmd, &mut stack, statement, run_label, line),
+            // "split" => operations::split(cmd, statement, &mut local_vars, run_label, line),
+            // "parse" => stdfn::parse_int(&mut local_vars, cmd, run_label, line),
+            // "vec" => {
+            //     operations::vec_ops(&mut stack, cmd, statement, &mut local_vars, run_label, line)
+            // }
+            "cmp" => operations::cmp(memory, vars, &mut local_registers, cmd, run_label, line),
+            "je" => jump::je(
+                memory,
+                vars,
+                local_registers.clone(),
+                instructions.clone(),
+                cmd,
+                run_label,
+                line,
+            ),
+            "jne" => jump::jne(
+                memory,
+                vars,
+                local_registers.clone(),
+                instructions.clone(),
+                cmd,
+                run_label,
+                line,
+            ),
+            "jgr" => jump::jgr(
+                memory,
+                vars,
+                local_registers.clone(),
+                instructions.clone(),
+                cmd,
+                run_label,
+                line,
+            ),
+            "jsm" => jump::jsm(
+                memory,
+                vars,
+                local_registers.clone(),
+                instructions.clone(),
+                cmd,
+                run_label,
+                line,
+            ),
+            "jmp" => jump::jmp(
+                memory,
+                vars,
+                local_registers.clone(),
+                instructions.clone(),
+                cmd,
+                run_label,
+                line,
+            ),
+            "halt" => process::exit(0),
+
+            _ => {
+                println!(
+                    "Cant recognize command '{}' at '{}' line: {}",
+                    cmd[0], run_label, line
+                );
+                process::exit(1)
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,190 +1,34 @@
 use std::collections::HashMap;
+use std::fs::File;
+use std::io;
 use std::io::prelude::*;
-use std::{env, fs, io, process};
 
-mod memory;
-mod types;
+use ram::execute_block;
+use ram::memory::Memory;
+use ram::parser;
+use ram::types::Vars;
+use ram::Registers;
 
-mod funcs;
-mod parser;
-use memory::Memory;
-use types::Vars;
+fn main() {
+    let mut filename: String = String::new();
+    println!("Welcome to the Ram stack-based programming language.");
+    println!("Please enter a filename: ");
+    io::stdin()
+        .read_line(&mut filename)
+        .expect("Error reading filename");
+    filename = filename.trim().to_string();
 
-// use funcs::{errors, jump, operations, print, stack, stdfn, var};
-use funcs::{jump, operations, print, stack};
-
-#[derive(Debug, Clone)]
-pub struct Registers {
-    pub lx: i32,
-    pub rv: i32,
-    pub string: String,
-    pub lxstring: String,
-}
-
-fn main() -> std::io::Result<()> {
-    let mut filename = String::new();
-    if env::args().nth(1).is_none() {
-        println!("Welcome to the Ram stack-based programming language.");
-        println!("Please enter a filename: ");
-        io::stdin().read_line(&mut filename)?;
-        filename = filename.trim().to_string();
-    } else {
-        if env::args().nth(1).unwrap() == "test" {
-        } else {
-            filename = env::args().nth(1).unwrap();
-        }
-    }
-
-    let mut file = fs::File::open(filename.trim())?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    let mut file: File = File::open(filename.trim()).expect("Error opening file");
+    let mut contents: String = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Error reading file");
 
     let p_lines: Vec<&str> = contents.split("\n").collect();
-    let program: HashMap<String, Vec<String>> = parser::parse_lines(p_lines);
+    let instructions: HashMap<String, Vec<String>> = parser::parse_lines(p_lines);
 
-    let registers = Registers {
-        lx: 0,
-        rv: 0,
-        string: String::from(""),
-        lxstring: String::from(""),
-    };
+    let registers: Registers = Registers::new();
     let mut memory: Memory = Memory::new();
     let mut vars: Vars = Vars::new();
 
-    execute_block(program, "main:", registers, &mut memory, &mut vars);
-    Ok(())
-}
-
-pub fn execute_block(
-    program: HashMap<String, Vec<String>>,
-    run_label: &str,
-    registers: Registers,
-    memory: &mut Memory,
-    vars: &mut Vars,
-) {
-    let mut line = 0i32;
-
-    let run_block: Vec<String>;
-    match program.get(run_label) {
-        Some(value) => run_block = value.to_vec(),
-        _ => {
-            println!("label '{}' not found", run_label);
-            process::exit(1);
-        }
-    }
-
-    let mut local_registers = Registers {
-        lx: registers.lx,
-        rv: registers.rv,
-        string: registers.string,
-        lxstring: registers.lxstring,
-    };
-
-    let block_ref = run_block.clone();
-
-    for statement in run_block {
-        let statement = statement.trim();
-        line += 1;
-        let cmd: Vec<&str> = statement.split_whitespace().collect();
-        match cmd[0] {
-            "dbg" => {
-                println!("Stack: ");
-                for i in memory.stack.chunks(8) {
-                    println!("{:?}", i)
-                }
-                println!(
-                    "{:#?} \n {:#?} \n HEAP: {:#?}",
-                    vars, local_registers, memory.heap
-                );
-
-                println!("{:?}", block_ref);
-            }
-            "print" => print::print(memory, vars, &mut local_registers, cmd, run_label, line),
-            "ram" => stack::ram(
-                memory,
-                vars,
-                &mut local_registers,
-                cmd,
-                statement,
-                run_label,
-                line,
-            ),
-            "reset" => memory.reset_stack(),
-            // "move" => var::movefn(cmd, &mut local_vars, hash_vars, run_label, line),
-            // "str" => stack::strfn(&mut stack, &mut local_vars, cmd, run_label, line),
-            // "stdin" => stdfn::stdin(&mut local_vars, cmd, run_label, line),
-            // "stdfs" => stdfn::stdfs(&mut local_vars, cmd, statement, run_label, line),
-            // "pop" => stack::pop(&mut stack, cmd, run_label, line),
-            // // "popall" => stack = vec![],
-            // "add" => operations::add(&mut stack, cmd, &mut local_vars, run_label, line),
-            // "sub" => operations::sub(&mut stack, run_label, line),
-            // "mul" => operations::mul(&mut stack, cmd, &mut local_vars, run_label, line),
-            // "div" => operations::div(&mut stack, run_label, line),
-            // "sqr" => operations::sqr(&mut stack, cmd, &mut local_vars, run_label, line),
-            // "sqrt" => operations::sqrt(&mut stack, cmd, &mut local_vars, run_label, line),
-            // "round" => operations::round(&mut stack, cmd, &mut local_vars, run_label, line),
-            // "avg" => operations::avg(&mut stack, run_label, line),
-            // "rand" => stdfn::random(&mut local_vars, cmd, &mut stack, statement, run_label, line),
-            // "split" => operations::split(cmd, statement, &mut local_vars, run_label, line),
-            // "parse" => stdfn::parse_int(&mut local_vars, cmd, run_label, line),
-            // "vec" => {
-            //     operations::vec_ops(&mut stack, cmd, statement, &mut local_vars, run_label, line)
-            // }
-            "cmp" => operations::cmp(memory, vars, &mut local_registers, cmd, run_label, line),
-            "je" => jump::je(
-                memory,
-                vars,
-                local_registers.clone(),
-                program.clone(),
-                cmd,
-                run_label,
-                line,
-            ),
-            "jne" => jump::jne(
-                memory,
-                vars,
-                local_registers.clone(),
-                program.clone(),
-                cmd,
-                run_label,
-                line,
-            ),
-            "jgr" => jump::jgr(
-                memory,
-                vars,
-                local_registers.clone(),
-                program.clone(),
-                cmd,
-                run_label,
-                line,
-            ),
-            "jsm" => jump::jsm(
-                memory,
-                vars,
-                local_registers.clone(),
-                program.clone(),
-                cmd,
-                run_label,
-                line,
-            ),
-            "jmp" => jump::jmp(
-                memory,
-                vars,
-                local_registers.clone(),
-                program.clone(),
-                cmd,
-                run_label,
-                line,
-            ),
-            "halt" => process::exit(0),
-
-            _ => {
-                println!(
-                    "Cant recognize command '{}' at '{}' line: {}",
-                    cmd[0], run_label, line
-                );
-                process::exit(1)
-            }
-        }
-    }
+    execute_block(instructions, "main:", registers, &mut memory, &mut vars);
 }
