5bb259f3c2c8697e09e6200ad70dca1d6e863f9f
Ujjwal <epicujjwal@protonmail.com>
2022-10-20 10:26:24 +0530

    main: fix env arg

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
+use std::{env, io};
 
 use ram::execute_block;
 use ram::memory::Memory;
@@ -11,13 +11,16 @@ use ram::Registers;
 
 fn main() {
     let mut filename: String = String::new();
-    println!("Welcome to the Ram stack-based programming language.");
-    println!("Please enter a filename: ");
-    io::stdin()
-        .read_line(&mut filename)
-        .expect("Error reading filename");
-    filename = filename.trim().to_string();
-
+    if env::args().nth(1).is_none() {
+        println!("Welcome to the Ram stack-based programming language.");
+        println!("Please enter a filename: ");
+        io::stdin()
+            .read_line(&mut filename)
+            .expect("Error reading filename");
+        filename = filename.trim().to_string();
+    } else {
+        filename = env::args().nth(1).unwrap()
+    }
     let mut file: File = File::open(filename.trim()).expect("Error opening file");
     let mut contents: String = String::new();
     file.read_to_string(&mut contents)
