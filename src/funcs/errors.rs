dbb97ee7a394c5eb22c6c896b16ce1b4d94daad5
Ujjwal <epicujjwal@protonmail.com>
2022-11-18 07:19:51 +0000

    errors: implement get_label

--- src/funcs/errors.rs
+++ src/funcs/errors.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::{collections::HashMap, process};
 
 pub fn stack_len_error(run_label: &str, line: i32) {
     println!(
@@ -56,4 +56,31 @@ pub fn vec_items(run_label: &str, line: u32) {
         line.to_string()
     );
     process::exit(1)
+}
+
+fn get_label(pc: u32, map: HashMap<String, usize>) -> String {
+    let mut label_point_vec = map.values().cloned().collect::<Vec<usize>>();
+    label_point_vec.sort();
+    let final_point: usize;
+    let mut final_label = String::from("");
+    let mut point_stack: Vec<usize> = vec![];
+    for point in label_point_vec {
+        if pc >= point as u32 {
+            point_stack.push(point)
+        }
+    }
+    final_point = point_stack[point_stack.len() -1];
+    let key_vec = map.keys().cloned().collect::<Vec<String>>();
+    for k in key_vec {
+        match map.get(&k) {
+            Some(&n) => {
+                if n == final_point {
+                    final_label = k;
+                    break;
+                }
+            },
+            None => panic!("nani?"),
+        }
+    }
+    final_label
 }
